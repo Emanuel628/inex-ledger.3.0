13087033c0d4b0f914e6959a9b4086b10553813c
Emanuel Castro <144055605+Emanuel628@users.noreply.github.com>
2026-07-25 18:41:33 -0400

    Split billing and theme styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
+import './styles/billing-subscription.css'
+import './styles/theme-dark.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
